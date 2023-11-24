ca993a9569f2842e9dc2ef2982e67e3c701b49ab
chinwe-regina <chinweregina21@gmail.com>
2023-11-24 13:49:26 +0100

    dbConfig

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -3,5 +3,11 @@ import mongoose from "mongoose"
 const URl: string ="mongodb://127.0.01:27017"
 
 export const dbConfig =async ()=>{
-    
+    try{
+        await mongoose.connect(URL);
+        console.log("db connected");
+        
+    } catch (error) {
+        console.log(error);
+      }
 }
